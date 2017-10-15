20c4cd3de118855090753f4518abf49972cc6fa1
welxo88 <welxo88@gmail.com>
2017-10-15 21:49:55 +0300

    configuration for nodeJS and building + support files + start of refactoring

---
+++ src/components/RulesRoot.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class RulesRoot extends React.Component {
+    componentDidMount() {
+        document.title = "Rules - sääntöjä helpommin";
+        setAccordionScriptTag();
+    }
+    render() {
+        return (
+            <div>
+                <br />
+                
+            </div>
+        );
+    }
+}
+
+export default RulesRoot;
