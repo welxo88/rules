3bc94836f2f4903eec50cdc6bcfbbbc50dbcedc3
welxo88 <welxo88@gmail.com>
2017-10-15 22:27:42 +0300

    cleanup

--- scripts/server.js
+++ scripts/server.js
@@ -1,7 +1,3 @@
-// src/server.js
-
-
-const express = require('express');
 const path = require('path');
 const app = express();
 
@@ -13,80 +9,12 @@ app.get('/', function (req, res) {
 
 app.get('/bundle.js', function (req, res) {
     res.sendFile(path.join(__dirname, '/../build', 'bundle.js'));
-  });
+});
+
+app.get('/bundle.js', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../build', 'example.json'));
+});
 
 app.listen(9000);
+
 console.log('Server running at http://127.0.0.1:9000/');
-
-/*
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:1337/');
-*/
-/**
-const path = require('path');
-const { Server } = require('http');
-const Express = require('express');
-const React = require('react');
-const { renderToString } = require('react-dom/server');
-const { match, RouterContext } = require('react-router');
-const routes = require('./routes');
-const NotFoundPage = require('./components/NotFoundPage');
-
-// initialize the server and configure support for ejs templates
-const app = new Express();
-const server = new Server(app);
-//app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-// define the folder that will be used for static assets
-app.use(Express.static(path.join(__dirname, 'static')));
-
-// universal routing and rendering
-app.get('*', (req, res) => {
-  match(
-    { routes, location: req.url },
-    (err, redirectLocation, renderProps) => {
-
-      // in case of error display the error message
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-
-      // in case of redirect propagate the redirect to the browser
-      if (redirectLocation) {
-        return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-      }
-
-      // generate the React markup for the current route
-      let markup;
-      if (renderProps) {
-        // if the current route matched we have renderProps
-        markup = renderToString(<RouterContext {...renderProps}/>);
-      } else {
-        // otherwise we can render a 404 page
-        markup = renderToString(<NotFoundPage/>);
-        res.status(404);
-      }
-
-      // render the index template with the embedded React markup
-      return res.render('index', { markup });
-    }
-  );
-});
-
-// start the server
-const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
-server.listen(port, err => {
-  if (err) {
-    return console.error(err);
-  }
-  console.info(`Server running on http://localhost:${port} [${env}]`);
-});
-*/
