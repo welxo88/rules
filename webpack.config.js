68b246e682e24ea57c7610fc34f4cc12b635a406
welxo88 <welxo88@gmail.com>
2017-10-15 22:19:12 +0300

    config update + delete empty file

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,10 @@ var config = {
       {
         test : /\.jsx?/,
         include : APP_DIR,
-        loader : 'babel-loader'
+        loader : 'babel-loader',
+        query: {
+            presets: ['es2015', 'react'],
+          }
       }
     ]
   }
