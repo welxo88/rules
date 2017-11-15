a68165549ce038bb00a86b3bf086fbaa553fb086
welxo88 <welxo88@gmail.com>
2017-11-15 22:54:19 +0200

    got encoding correctly

--- src/backend/createJSON.php
+++ src/backend/createJSON.php
@@ -6,7 +6,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // check connection
 if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error);}
 
-$sql = "
+$sql = <<<SQL
 SELECT 
 	rules.ruleId, rules.ruleTextId, rules.ruleName, rules.validStart, rules.validEnd, rules.approvedDate, rules.approvedBy, rules.linkToOriginal,
 	ruleSections.sectionId, ruleSections.parentSection, ruleSections.sectionHeader,
@@ -19,16 +19,25 @@ INNER JOIN
 	rules ON ruleSections.ruleId=rules.ruleId
 ORDER BY
 	LENGTH(rules.ruleId), rules.ruleId, LENGTH(ruleSections.sectionId), ruleSections.sectionId, LENGTH(ruleParagraph.paragraphId), ruleParagraph.paragraphId
-";
+SQL;
+
+if ($conn->query("SET CHARACTER SET utf8") === TRUE) {    //woop-woop
+}
 //get data from db
-$result = $conn->query($sql);
+if(!$result = $conn->query($sql)){
+    die('There was an error running the query [' . $conn->error . ']');
+}
+
+header("Content-Type: text/json; charset=utf-8");
 
 //organize info into array
 $return_arr = array();
 $row_array = array();
-while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+while ($row = $result->fetch_assoc()) {
+    
     $row_array['paragraphId'] = $row['paragraphId'];
     $row_array['paragraphText'] = $row['paragraphText'];
+    //$row_array['paragraphText'] = mb_convert_encoding($row['paragraphText'], "UTF-8", "UTF-8");    
     $row_array['paragraphInterpretation'] = $row['paragraphInterpretation'];
     $row_array['paragraphIsSub'] = $row['paragraphIsSub'];
     $row_array['paragraphIsPartOfList'] = $row['paragraphIsPartOfList'];
@@ -38,7 +47,7 @@ while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
 }
 
 //print array out as json
-echo json_encode($return_arr);
+echo json_encode($return_arr, JSON_UNESCAPED_UNICODE);
 
 $conn->close();
 ?> 
