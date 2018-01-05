b5b7eb12c45c73fb8bef35d0454db480041e361f
welxo88 <welxo88@gmail.com>
2018-01-05 12:01:12 +0200

    date changes to human readable applied

--- src/components/RuleBody.js
+++ src/components/RuleBody.js
@@ -5,9 +5,15 @@ class RuleBody extends React.Component {
     constructor(props) {
         super(props);
         this.trueArrayChecker = this.trueArrayChecker.bind(this);
+        this.dbDateToHumanRead = this.dbDateToHumanRead.bind(this);
     }
 
     trueArrayChecker(el,index,array){return el==true;}
+
+    dbDateToHumanRead(date){
+        let stamp = new Date(Date.parse(date));
+        return stamp.getDate()+'.'+(stamp.getMonth()+1)+'.'+stamp.getFullYear();
+    }
 
     createSections(sections){
         return sections.map((section,index) =>{
@@ -42,13 +48,13 @@ class RuleBody extends React.Component {
                 <div className={"title " + (isFound ? 'active' : '')}>
                     <i className="dropdown icon"></i>
                     {this.props.data.ruleName!=undefined && this.props.data.ruleName + ", "}
-                    {this.props.data.validStart!=undefined && this.props.data.validStart}
+                    {this.props.data.validStart!=undefined && this.dbDateToHumanRead(this.props.data.validStart)}
                 </div>
                 <div className={"content " + (isFound ? 'active' : '')}>
-                    {this.props.data.approvedDate!=undefined && 'Hyväksytty: '+this.props.data.approvedDate}
+                    {this.props.data.approvedDate!=undefined && 'Hyväksytty: '+this.dbDateToHumanRead(this.props.data.approvedDate)}
                     <br />
-                    {this.props.data.validStart!=undefined && ' Voimassaolo: '+this.props.data.validStart+' - '}
-                    {this.props.data.validEnd!=undefined && this.props.data.validEnd}
+                    {this.props.data.validStart!=undefined && ' Voimassaolo: '+this.dbDateToHumanRead(this.props.data.validStart)+' - '}
+                    {this.props.data.validEnd!=undefined && this.dbDateToHumanRead(this.props.data.validEnd)}
                     <br />
                     {this.props.data.linkToOriginal!=undefined && <a href={this.props.data.linkToOriginal} target='_blank'>Alkuperäinen teksti (pdf)</a>}
                     <div className="accordion">
